df9867974cad2fe610ba0056a5588bb89e7ce129
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2023-12-10 23:11:32 -0600

    [tailwind] Add color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,13 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    colors: {
+      'text': '#eee2e2',
+      'bg': '#333333',
+      'primary': '#de9699',
+      'secondary': '#872626',
+      'accent': '#990000'
+    }
+  }
 } satisfies Config;
