ac901f2ac4bdf5f0353b25a2c918cf8d415c39a9
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-11-24 10:18:58 -0600

    [/(root)] Rm react vite

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
-Deno.serve((req) => {
-  return serveDir(req, {
-    fsRoot: "dist", // serve your Vite build
-    urlRoot: "", // serve from root
-    enableCors: true,
-  });
-});
