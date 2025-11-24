9ce07cd543d68ac7720429ec13392c4749fcde8c
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-11-24 10:24:13 -0600

    [/(root)] init deno fresh v2 with tailwindcss

---
+++ routes/index.tsx
@@ -0,0 +1,27 @@
+import { useSignal } from "@preact/signals";
+import { Head } from "fresh/runtime";
+import { define } from "../utils.ts";
+import Counter from "../islands/Counter.tsx";
+
+export default define.page(function Home(ctx) {
+  const count = useSignal(3);
+
+  return (
+    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+      <Head>
+        <title>Fresh counter</title>
+      </Head>
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl font-bold">Welcome to Marshellyan.com</h1>
+        <Counter count={count} />
+      </div>
+    </div>
+  );
+});
